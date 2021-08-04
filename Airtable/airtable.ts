118245630cd3de06d98bf51985fadc9669a8e9a0
romarious <romarious.riazanov@gmail.com>
2021-08-04 12:57:02 +0300

    1 file modified

--- Airtable/airtable.ts
+++ Airtable/airtable.ts
@@ -11,36 +11,6 @@ export const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(
 
 export let currentRecordId: string = '';
 
-export const getFormSubmitions = () => {
-  const forms = base(AIRTABLE_NAME)
-    .select({
-      // Selecting the first 3 records in Grid view:
-      maxRecords: 5,
-      view: 'Grid view'
-    })
-    .eachPage(
-      function page(records, fetchNextPage) {
-        // This function (`page`) will get called for each page of records.
-
-        records.forEach(function(record) {
-          console.log('Retrieved', record.fields);
-        });
-
-        // To fetch the next page of records, call `fetchNextPage`.
-        // If there are more records, `page` will get called again.
-        // If there are no more records, `done` will get called.
-        fetchNextPage();
-      },
-      function done(err) {
-        if (err) {
-          console.error(err);
-          return;
-        }
-      }
-    );
-  return forms;
-};
-
 export type FieldRecord = {
   Title: string;
   Description: string;
@@ -49,14 +19,17 @@ export type FieldRecord = {
 };
 
 export const createRecord = async (fields: FieldRecord) => {
-  await base(AIRTABLE_NAME).create({ ...fields }, function(err, record) {
+  const res = await base(AIRTABLE_NAME).create({ ...fields }, function(
+    err,
+    record
+  ) {
     if (err) {
       console.error(err);
       return;
     }
     currentRecordId = record.id;
-    return currentRecordId;
   });
+  return res;
 };
 
 export const updateRecord = (fields: FieldRecord) => {
